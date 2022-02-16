4d10ef09a69c955b3adbee2a46de12677b5dbf08
shiyanshirani <shiyan99s@gmail.com>
2022-02-16 23:25:35 +0530

    suffix changed to api/

--- Spinny/urls.py
+++ Spinny/urls.py
@@ -19,6 +19,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("box/", include("boxes.urls")),
+    path("api/", include("boxes.urls")),
     path("auth_token/", obtain_auth_token, name="auth-token"),
 ]
